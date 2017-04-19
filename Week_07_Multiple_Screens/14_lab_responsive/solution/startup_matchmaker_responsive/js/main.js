e55fda900143cb6c515c786602fe660ef593e31b
James Gallichio <james@plattar.com>
2017-04-19 20:36:13 +1000

    Fixed crappy menu toggle

--- Week_07_Multiple_Screens/14_lab_responsive/solution/startup_matchmaker_responsive/js/main.js
+++ Week_07_Multiple_Screens/14_lab_responsive/solution/startup_matchmaker_responsive/js/main.js
@@ -1,8 +1,3 @@
-document.getElementsByTagName("li")[0].addEventListener("click",function(){
-	if(!this.parentNode.getAttribute("class")){
-		this.parentNode.setAttribute("class","slideDown");
-	}
-	else{
-		this.parentNode.removeAttribute("class");
-	}
-});
+$('.hamburger').click(function(){
+  $('.menu').toggleClass('open');
+})
